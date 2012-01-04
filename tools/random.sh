4ff3eb03e596d842842a3ac34b6d5fa7e05afaec
Lars Lindner <lars.lindner@gmail.com>
2012-01-04 23:30:32 +0100

    Improved random request script.

--- tools/random.sh
+++ tools/random.sh
@@ -25,13 +25,28 @@ if [ "$1" == "" ]; then
 	done
 else 
 	theHost=$1
-	ctxt=$(($RANDOM % 100))
+	ctxt=$$_$(($RANDOM % 100))
 	nr=1
 
 	# Worker mode
 	while [ 1 ]; do
 		sleep $(($RANDOM % 20 + 3))
-		curl -s "$SERVER/set?host=$theHost&component=comp&time=$(date +%s)&type=n&ctxt=${ctxt}_${nr}&status=running&desc=test+request"
+		curl -s "$SERVER/set?host=$theHost&component=comp&time=$(date +%s)&type=n&ctxt=${ctxt}_${nr}&status=started&desc=test+request"
+
+		sleep 2
+		
+		steps=$(($RANDOM % 5 + 1))
+		i=0
+		while [ $i -lt $steps ];
+		do
+			i=$(($i + 1))
+			sleep $(($RANDOM % 3 + 1))
+			curl -s "$SERVER/set?host=$theHost&component=comp&time=$(date +%s)&type=n&ctxt=${ctxt}_${nr}&status=running&desc=step $i/$steps"
+		done
+
+		sleep 2
+
+		curl -s "$SERVER/set?host=$theHost&component=comp&time=$(date +%s)&type=n&ctxt=${ctxt}_${nr}&status=finished&desc=test+request+done"
 		nr=$(($nr + 1))
 	done
 fi
